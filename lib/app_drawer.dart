54487fd393fcbbde8ed693fabc0597ae364c6bfd
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2018-04-12 20:05:06 +0200

    working on drawe menu

--- lib/app_drawer.dart
+++ lib/app_drawer.dart
@@ -2,6 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:startup_namer/callable.dart';
 
+class MenuEntry {
+  final String title;
+  final String sport;
+  final String league;
+  final String region;
+  final int count;
+
+  MenuEntry({this.title, this.sport, this.league, this.region, this.count = 0});
+}
+
+final List<MenuEntry> higlights = [
+  new MenuEntry(title: "Home"),
+  new MenuEntry(title: "Live Right Now"),
+  new MenuEntry(title: "Starting Soon"),
+];
+
+final popular = [
+  new MenuEntry(sport: "Football", region: "Sweden", league: "Allsvenskan", count: 1569),
+  new MenuEntry(sport: "Football", region: "Sweden", league: "Superettan", count: 1005),
+  new MenuEntry(sport: "Football", region: "England", league: "Premier League", count: 3987),
+  new MenuEntry(sport: "Football", region: "Italy", league: "Serie A", count: 1178),
+  new MenuEntry(sport: "Football", region: "Spain", league: "LaLiga", count: 2236),
+  new MenuEntry(sport: "Ice Hockey", region: "Sweden", league: "SHL", count: 111),
+  new MenuEntry(sport: "Ice Hockey", league: "NHL", count: 1231),
+  new MenuEntry(sport: "Basketball", league: "NBA", count: 10),
+  new MenuEntry(sport: "Tennis", count: 182),
+];
+
+final sports = [
+  new MenuEntry(sport: "Football", count: 73020),
+  new MenuEntry(sport: "Ice Hockey", count: 2545),
+  new MenuEntry(sport: "Tennis", count: 182),
+  new MenuEntry(sport: "Trotting", count: 92),
+  new MenuEntry(sport: "Golf", count: 40),
+  new MenuEntry(sport: "Basketball", count: 2541),
+  new MenuEntry(sport: "Handball", count: 347),
+  new MenuEntry(sport: "UFC/MMA", count: 180),
+];
+
+final moreSports = [
+  new MenuEntry(sport: "American Fotball", count: 14),
+  new MenuEntry(sport: "Australian Rules", count: 1180),
+  new MenuEntry(sport: "Baseball", count: 427),
+  new MenuEntry(sport: "Boxing", count: 50),
+  new MenuEntry(sport: "Chess", count: 3),
+  new MenuEntry(sport: "Commonswealth Games", count: 236),
+  new MenuEntry(sport: "Cricket", count: 105),
+  new MenuEntry(sport: "Darts", count: 232),
+  new MenuEntry(sport: "Floorball", count: 28),
+  new MenuEntry(sport: "Futsal", count: 17),
+  new MenuEntry(sport: "Gaelic Sports", count: 21),
+  new MenuEntry(sport: "Greyhounds", count: 207),
+  new MenuEntry(sport: "Horse Racing", count: 209),
+  new MenuEntry(sport: "Motorsports", count: 58),
+  new MenuEntry(sport: "Netball", count: 1),
+  new MenuEntry(sport: "Olympic Games", count: 1),
+  new MenuEntry(sport: "Pesapallo", count: 1),
+  new MenuEntry(sport: "Politics", count: 51),
+  new MenuEntry(sport: "Rugby League", count: 1041),
+  new MenuEntry(sport: "Rugby Union", count: 483),
+  new MenuEntry(sport: "Snooker", count: 96),
+  new MenuEntry(sport: "Surfing", count: 22),
+  new MenuEntry(sport: "TV & Novelty", count: 22),
+  new MenuEntry(sport: "Volleyball", count: 22),
+  new MenuEntry(sport: "Winter Olympic Games", count: 1),
+  new MenuEntry(sport: "Winter Sports", count: 7),
+];
+
 class AppDrawer extends StatelessWidget {
   final String title;
   final Callable<Widget> onSelect;
@@ -11,132 +79,50 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Drawer(
-      child: new ContactsDemo(),
-    );
-  }
-
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Drawer(
-//      child: new ListView(
-//        padding: EdgeInsets.zero,
-//        children: <Widget>[
-//          new Stack(
-//            alignment: const Alignment(0.6, 0.6),
-//            children: <Widget>[
-//              Image.asset(
-//                "assets/banner_1.jpg",
-//                fit: BoxFit.cover,
-//                height: 100.0,
-//              ),
-//              new Text(
-//                "PLAY",
-//                style: new TextStyle(
-//                  fontSize: 20.0,
-//                  fontWeight: FontWeight.bold,
-//                  color: Colors.white,
-//                ),
-//              )
-//            ],
-//          ),
-////          new DrawerHeader(
-////            child: new Text('Svan Play!'),
-////            decoration: new BoxDecoration(
-////                image: new DecorationImage(
-////                    image: new AssetImage("assets/banner_1.jpg"))),
-////          ),
-//          new ListTile(
-//            title: new Text('Start Page'),
-//            onTap: () {
-//              onSelect(new StartPage());
-//              Navigator.pop(context);
-//            },
-//          ),
-//          new ListTile(
-//            title: new Text('Sport Page'),
-//            onTap: () {
-//              onSelect(new SportPage());
-//              Navigator.pop(context);
-//            },
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-}
-
-class _ContactCategory extends StatelessWidget {
-  const _ContactCategory({Key key, this.icon, this.children}) : super(key: key);
-
-  final IconData icon;
-  final List<Widget> children;
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    return new Container(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      decoration: new BoxDecoration(border: new Border(bottom: new BorderSide(color: themeData.dividerColor))),
-      child: new DefaultTextStyle(
-        style: Theme.of(context).textTheme.subhead,
-        child: new SafeArea(
-          top: false,
-          bottom: false,
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Container(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0), width: 72.0, child: new Icon(icon, color: themeData.primaryColor)),
-              new Expanded(child: new Column(children: children))
-            ],
-          ),
-        ),
-      ),
+      child: new AppMenu(),
     );
   }
 }
 
-class _ContactItem extends StatelessWidget {
-  _ContactItem({Key key, this.icon, this.lines, this.tooltip, this.onPressed})
-      : assert(lines.length > 1),
-        super(key: key);
+class MenuEntryRow extends StatelessWidget {
+  final MenuEntry entry;
 
-  final IconData icon;
-  final List<String> lines;
-  final String tooltip;
-  final VoidCallback onPressed;
+  MenuEntryRow(this.entry);
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    final List<Widget> columnChildren = lines.sublist(0, lines.length - 1).map((String line) => new Text(line)).toList();
-    columnChildren.add(new Text(lines.last, style: themeData.textTheme.caption));
-
-    final List<Widget> rowChildren = <Widget>[
-      new Expanded(child: new Column(crossAxisAlignment: CrossAxisAlignment.start, children: columnChildren))
-    ];
-    if (icon != null) {
-      rowChildren
-          .add(new SizedBox(width: 72.0, child: new IconButton(icon: new Icon(icon), color: themeData.primaryColor, onPressed: onPressed)));
+    var theme = Theme.of(context);
+    List<Widget> row = [];
+    if (entry.title != null) {
+      row.add(new Text(entry.title));
+    } else if (entry.league != null) {
+      row.add(new Text(entry.league));
+      row.add(new Padding(
+          padding: EdgeInsets.only(left: 4.0), child: new Text(entry.sport, style: theme.textTheme.caption)));
+      if (entry.region != null) {
+        row.add(new Padding(
+            padding: EdgeInsets.only(left: 2.0), child: new Text("/", style: theme.textTheme.caption)));
+        row.add(new Padding(
+            padding: EdgeInsets.only(left: 2.0), child: new Text(entry.region, style: theme.textTheme.caption)));
+      }
+    } else {
+      row.add(new Text(entry.sport));
     }
-    return new MergeSemantics(
-      child: new Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: new Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: rowChildren)),
-    );
+
+    return new Padding(
+        padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
+        child: Row(
+          children: <Widget>[
+            new Expanded(child: new Row(children: row, crossAxisAlignment: CrossAxisAlignment.end)),
+            new Text(entry.count.toString(), style: Theme.of(context).textTheme.caption)
+          ],
+        ));
   }
-}
-
-class ContactsDemo extends StatefulWidget {
-  static const String routeName = '/contacts';
-
-  @override
-  ContactsDemoState createState() => new ContactsDemoState();
 }
 
 enum AppBarBehavior { normal, pinned, floating, snapping }
 
-class ContactsDemoState extends State<ContactsDemo> {
+class AppMenu extends StatelessWidget {
   static final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   final double _appBarHeight = 150.0;
 
@@ -146,7 +132,7 @@ class ContactsDemoState extends State<ContactsDemo> {
   Widget build(BuildContext context) {
     return new Theme(
       data: new ThemeData(
-        brightness: Brightness.light,
+        brightness: Brightness.dark,
         primarySwatch: Colors.blueGrey,
         platform: Theme.of(context).platform,
       ),
@@ -159,29 +145,6 @@ class ContactsDemoState extends State<ContactsDemo> {
               pinned: _appBarBehavior == AppBarBehavior.pinned,
               floating: _appBarBehavior == AppBarBehavior.floating || _appBarBehavior == AppBarBehavior.snapping,
               snap: _appBarBehavior == AppBarBehavior.snapping,
-//              actions: <Widget>[
-//                new IconButton(
-//                  icon: const Icon(Icons.create),
-//                  tooltip: 'Edit',
-//                  onPressed: () {
-//                    _scaffoldKey.currentState
-//                        .showSnackBar(const SnackBar(content: const Text('This is actually just a demo. Editing isn\'t supported.')));
-//                  },
-//                ),
-//                new PopupMenuButton<AppBarBehavior>(
-//                  onSelected: (AppBarBehavior value) {
-//                    setState(() {
-//                      _appBarBehavior = value;
-//                    });
-//                  },
-//                  itemBuilder: (BuildContext context) => <PopupMenuItem<AppBarBehavior>>[
-//                        const PopupMenuItem<AppBarBehavior>(value: AppBarBehavior.normal, child: const Text('App bar scrolls away')),
-//                        const PopupMenuItem<AppBarBehavior>(value: AppBarBehavior.pinned, child: const Text('App bar stays put')),
-//                        const PopupMenuItem<AppBarBehavior>(value: AppBarBehavior.floating, child: const Text('App bar floats')),
-//                        const PopupMenuItem<AppBarBehavior>(value: AppBarBehavior.snapping, child: const Text('App bar snaps')),
-//                      ],
-//                ),
-//              ],
               flexibleSpace: new FlexibleSpaceBar(
                 title: const Text('Svan Play!'),
                 background: new Stack(
@@ -192,167 +155,12 @@ class ContactsDemoState extends State<ContactsDemo> {
                       fit: BoxFit.cover,
                       height: _appBarHeight,
                     ),
-                    // This gradient ensures that the toolbar icons are distinct
-                    // against the background image.
-//                    const DecoratedBox(
-//                      decoration: const BoxDecoration(
-//                        gradient: const LinearGradient(
-//                          begin: const Alignment(0.0, -1.0),
-//                          end: const Alignment(0.0, -0.4),
-//                          colors: const <Color>[const Color(0x60000000), const Color(0x00000000)],
-//                        ),
-//                      ),
-//                    ),
                   ],
                 ),
               ),
             ),
             new SliverList(
-              delegate: new SliverChildListDelegate(<Widget>[
-                new _ContactCategory(
-                  icon: Icons.call,
-                  children: <Widget>[
-                    new _ContactItem(
-                      icon: Icons.message,
-                      tooltip: 'Send message',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('Pretend that this opened your SMS application.')));
-                      },
-                      lines: const <String>[
-                        '(650) 555-1234',
-                        'Mobile',
-                      ],
-                    ),
-                    new _ContactItem(
-                      icon: Icons.message,
-                      tooltip: 'Send message',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('In this demo, this button doesn\'t do anything.')));
-                      },
-                      lines: const <String>[
-                        '(323) 555-6789',
-                        'Work',
-                      ],
-                    ),
-                    new _ContactItem(
-                      icon: Icons.message,
-                      tooltip: 'Send message',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('Imagine if you will, a messaging application.')));
-                      },
-                      lines: const <String>[
-                        '(650) 555-6789',
-                        'Home',
-                      ],
-                    ),
-                  ],
-                ),
-                new _ContactCategory(
-                  icon: Icons.contact_mail,
-                  children: <Widget>[
-                    new _ContactItem(
-                      icon: Icons.email,
-                      tooltip: 'Send personal e-mail',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('Here, your e-mail application would open.')));
-                      },
-                      lines: const <String>[
-                        'ali_connors@example.com',
-                        'Personal',
-                      ],
-                    ),
-                    new _ContactItem(
-                      icon: Icons.email,
-                      tooltip: 'Send work e-mail',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('This is a demo, so this button does not actually work.')));
-                      },
-                      lines: const <String>[
-                        'aliconnors@example.com',
-                        'Work',
-                      ],
-                    ),
-                  ],
-                ),
-                new _ContactCategory(
-                  icon: Icons.location_on,
-                  children: <Widget>[
-                    new _ContactItem(
-                      icon: Icons.map,
-                      tooltip: 'Open map',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('This would show a map of San Francisco.')));
-                      },
-                      lines: const <String>[
-                        '2000 Main Street',
-                        'San Francisco, CA',
-                        'Home',
-                      ],
-                    ),
-                    new _ContactItem(
-                      icon: Icons.map,
-                      tooltip: 'Open map',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('This would show a map of Mountain View.')));
-                      },
-                      lines: const <String>[
-                        '1600 Amphitheater Parkway',
-                        'Mountain View, CA',
-                        'Work',
-                      ],
-                    ),
-                    new _ContactItem(
-                      icon: Icons.map,
-                      tooltip: 'Open map',
-                      onPressed: () {
-                        _scaffoldKey.currentState
-                            .showSnackBar(const SnackBar(content: const Text('This would also show a map, if this was not a demo.')));
-                      },
-                      lines: const <String>[
-                        '126 Severyns Ave',
-                        'Mountain View, CA',
-                        'Jet Travel',
-                      ],
-                    ),
-                  ],
-                ),
-                new _ContactCategory(
-                  icon: Icons.today,
-                  children: <Widget>[
-                    new _ContactItem(
-                      lines: const <String>[
-                        'Birthday',
-                        'January 9th, 1989',
-                      ],
-                    ),
-                    new _ContactItem(
-                      lines: const <String>[
-                        'Wedding anniversary',
-                        'June 21st, 2014',
-                      ],
-                    ),
-                    new _ContactItem(
-                      lines: const <String>[
-                        'First day in office',
-                        'January 20th, 2015',
-                      ],
-                    ),
-                    new _ContactItem(
-                      lines: const <String>[
-                        'Last day in office',
-                        'August 9th, 2015',
-                      ],
-                    ),
-                  ],
-                ),
-              ]),
+              delegate: new SliverChildListDelegate(popular.map((entry) => new MenuEntryRow(entry)).toList()),
             ),
           ],
         ),
