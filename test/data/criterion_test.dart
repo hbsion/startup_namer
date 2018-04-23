7cac07e88007dd4d670fd30f700a3f360df8c822
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2018-04-23 08:05:55 +0200

    added first unit test

---
+++ test/data/criterion_test.dart
@@ -0,0 +1,21 @@
+import 'package:startup_namer/data/criterion.dart';
+import 'package:test/test.dart';
+import 'dart:convert';
+
+
+main() {
+  test("should map json to criterion object", () {
+     var text = '''{
+     "id": 1001159858,
+     "label": "Full Time",
+     "englishLabel": "Full Time",
+     "order": [1,2,3]
+     }''';
+    var c = Criterion.fromJson(json.decode(text));
+
+    expect(c.id, 1001159858);
+    expect(c.label, "Full Time");
+    expect(c.order, [1,2,3]);
+  });
+
+}
